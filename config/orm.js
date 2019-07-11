dae70531b0a7142a6a6fc5f0cb8e99e6aae7ffcf
jebaquera <jebaquera@gmail.com>
2019-07-11 00:35:31 -0500

    updated all orm.

--- config/orm.js
+++ config/orm.js
@@ -35,7 +35,7 @@ function objToSql(ob) {
 // ------------------------------------------------
 // Object for all our SQL statement functions.
 var orm = {
-    selectAll: function(tableInput, cb) {
+    all: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
             if (err) {
@@ -45,7 +45,7 @@ var orm = {
         });
     },
 
-    insertOne: function(table, cols, vals, cb) {
+    create: function(table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
   
         queryString += " (";
@@ -66,7 +66,7 @@ var orm = {
         });
     },
     // An example of objColVals would be {name: cheeseburger}
-    updateOne: function(table, objColVals, condition, cb) {
+    update: function(table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
   
         queryString += " SET ";
