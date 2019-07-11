fd541b244b5ce253dbe1d218aeb5a3fd55047cb0
jebaquera <jebaquera@gmail.com>
2019-07-10 21:07:27 -0500

    completed initial DB setup.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Avocado and Cheddar');
+INSERT INTO burgers (name) VALUES ('Green Chile and Sauteed Onions');
+INSERT INTO burgers (name) VALUES ('Bacon and Cheddar Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Old-Fashioned Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Hickory Burger', true);
